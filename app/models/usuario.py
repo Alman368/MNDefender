42151d999e7c8bc7fddebebc6daba03e7d9e9c12
Alman368 <albertobartolomeiruela@gmail.com>
2025-05-08 23:27:04 +0200

    hay que unir user.py con usuario.py

--- app/models/usuario.py
+++ app/models/usuario.py
@@ -9,5 +9,5 @@ class Usuario(db.Model):
     apellidos = db.Column(db.String(100), nullable=False)
     correo = db.Column(db.String(120), unique=True, nullable=False)
     contrasena = db.Column(db.String(128), nullable=False)
-    fecha_creacion = db.Column(db.DateTime, default=datetime.utcnow)
-    fecha_modificacion = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+    fecha_creacion = db.Column(db.DateTime, default=datetime.now)
+    fecha_modificacion = db.Column(db.DateTime, default=datetime.now, onupdate=datetime.now)
